ae07288ff0e9809f80b5ff21af8dcb5c670c03b6
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-03-03 16:51:10 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+const start = dokument.querySelector("button[data-start]");
+const stop = document.querySelector("button[data-stop]");
+
+start.disabled = true;
+let colorInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+start.addIventListener('click', () => {
+    start.disabled = true;
+    stop.disabled = false;
+
+
+    colorInterval = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+}
+);
+
+stop.addIventListener("click", () => {
+    
+    clearInterval(colorInterval);
+
+    start.disabled = false; 
+    stop.disabled = true;
+    
+});
+
+
+
+
+
+
+
+
